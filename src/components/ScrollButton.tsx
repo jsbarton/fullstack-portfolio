d4abc3f7376add39a2e219059815c8fd38b3e0bd
jsbarton <jordansdbarton@gmail.com>
2024-05-30 08:53:21 -0400

    Add smooth scrolling scroll button, add projects section

--- src/components/ScrollButton.tsx
+++ src/components/ScrollButton.tsx
@@ -1,5 +1,25 @@
+import "/src/styles/ScrollButton.css";
+import { useRef } from "react";
+
 const ScrollButton = () => {
-  return <button>Click Me</button>;
+  const projectsRef = useRef<HTMLDivElement | null>(null);
+  return (
+    <>
+      <div className="button-container">
+        <div className="button bounce">
+          <button
+            onClick={() => {
+              console.log("button clicked");
+              projectsRef.current?.scrollIntoView({ behavior: "smooth" });
+            }}
+          ></button>
+        </div>
+      </div>
+      <div className="projects-frame" id="projects" ref={projectsRef}>
+        Projects
+      </div>
+    </>
+  );
 };
 
 export default ScrollButton;
