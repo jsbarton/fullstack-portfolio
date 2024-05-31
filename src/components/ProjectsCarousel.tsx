bb33e98932f012415d4a30df5ec4bce7312dd024
jsbarton <jordansdbarton@gmail.com>
2024-05-31 13:18:56 -0400

    Add wemetirl and placeholder img

--- src/components/ProjectsCarousel.tsx
+++ src/components/ProjectsCarousel.tsx
@@ -15,7 +15,7 @@ import {
 const projectDescription = [
   {
     title: "We Met IRL Website",
-    imgPath: "",
+    imgPath: "/wemetirlpreview.png",
     description:
       "Marketing website for a relationship building organization that hosts speed dating events, mixers, and social gatherigs, with divversity in mind",
     stack: "CSS, Squarespace dev",
@@ -23,7 +23,7 @@ const projectDescription = [
   },
   {
     title: "Mantra",
-    imgPath: "",
+    imgPath: "/favicon.svg", // remember to change this
     description:
       "Swift mobile app that provides users with daily, encouraging quotes",
     stack: "Swift",
@@ -41,6 +41,10 @@ const ProjectsCarousel = () => {
               <Card className="bg-slate-50 rounded-3xl ">
                 <CardContent className="flex flex-col justify-center p-6">
                   <CardTitle className="py-2">{description.title}</CardTitle>
+                  <img
+                    className="py-2.5"
+                    src={`../../${description.imgPath}`}
+                  />
                   <CardDescription className="py-1">
                     <span className="font-bold">{"Description: "}</span>
                     {description.description}
